34f44498d3da823bc0ba010d7a733d28ca7ea612
r1cardo360 <ric.furlanetto@hotmail.com>
2023-12-17 04:26:24 -0300

    Mudando o dicionario python para default

--- django2/settings.py
+++ django2/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'django2.wsgi.application'
 
 
 DATABASES = {
-    'defalt': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 '''
