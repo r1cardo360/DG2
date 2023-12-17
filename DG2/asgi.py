17a182634c69416035713988012dddcda07bfe96
r1cardo360 <ric.furlanetto@hotmail.com>
2023-12-17 05:00:23 -0300

    Mudando os arquivos de django2 para DG2

--- DG2/asgi.py
+++ DG2/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django2 project.
+ASGI config for DG2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DG2.settings')
 
 application = get_asgi_application()
--- DG2/wsgi.py
+++ DG2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django2 project.
+WSGI config for DG2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DG2.settings')
 
 application = get_wsgi_application()
